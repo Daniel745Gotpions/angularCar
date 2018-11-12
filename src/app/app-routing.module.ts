d534927c598f2dde16fe6dae54b206ea67a43d0a
danielb <daniel.berkovich@finantick.com>
2018-11-12 13:34:17 +0200

    In progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarsComponent }        from './cars/cars.component';
-//import { CarDetailsComponent }  from './car-details/car-details.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
 
 const routes: Routes = [
-	{ path: '', component: CarsComponent },
+	{ path: '',   redirectTo: '/car-list', pathMatch: 'full' },
+	{ path: 'car-details/:id', component: CarDetailsComponent },
 	{ path: 'car-list', component: CarsComponent },
-//	{ path: 'car-details/:id', component: CarDetailsComponent }
+	
 ];
 
 @NgModule({
--- src/app/car-details/car-details.component.ts
+++ src/app/car-details/car-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-car-details',
   templateUrl: './car-details.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { 
+		debugger;
+  }
+  
+
 
   ngOnInit() {
+  	debugger
+  	let id = parseInt(this.router);
   }
 
 }
